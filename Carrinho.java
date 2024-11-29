7432e0a978737ffb4305c4908d7fedec43629538
Carlos Felipe <felipevas.tech@gmail.com>
2024-11-29 19:49:13 -0300

    Create Carrinho.java

---
+++ Carrinho.java
@@ -0,0 +1,29 @@
+package LojaVirtual.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+    private List<Produto> produtos;
+
+    public Carrinho() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+        System.out.println(produto.getNome() + " adicionado ao carrinho.");
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Produto produto : produtos) {
+            total += produto.getPreco();
+        }
+        return total;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+}
