47a611171420174d5d3756221ef46c8ca225b882
danielhenrique <165221001+dhenriquea1419@users.noreply.github.com>
2024-11-29 17:23:16 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,28 @@
+package LojaVirtual;
+
+import LojaVirtual.modelo.*;
+import LojaVirtual.servico.GerenciadorProduto;
+
+public class Main {
+    public static void main(String[] args) {
+        GerenciadorProduto gerenciadorProduto = new Gerenciador
+
+        Produto p1 = new Produto(1, "Teclado", 50.0, 10);
+        Produto p2 = new Produto(2, "Rato", 30.0, 15);
+
+        gerenciadorProduto.adicionarProduto(p1);
+        gerenciadorProduto.adicionarProduto(p2);
+
+        System.out.println("Lista de produtos disponíveis:");
+        gerenciadorProduto.listarProdutos();
+
+        Cliente cliente = new Cliente(1, "João Silva", "joao@email.com");
+
+        Carrinho carrinho = new Carrinho();
+        carrinho.adicionarProduto(p1);
+        carrinho.adicionarProduto(p2);
+
+        Pedido pedido = new Pedido(cliente, carrinho);
+        pedido.finalizarPedido();
+    }
+}
