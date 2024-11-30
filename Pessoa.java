47d31a1ad6f5ccbdbfe8bf981f691bac8afd9338
GustavoAlbuq <167881515+GustavoAlbuq@users.noreply.github.com>
2024-11-29 22:30:18 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,25 @@
+public abstract class Pessoa {
+    protected int id;
+    protected String nome; 
+    protected String email;
+
+    public Pessoa(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void exibirInformacoes(); 
+}
